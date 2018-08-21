9a828f3e8d681937458ca4eafbfd45ce5316ece6
Vladimir Rech <rechvladimir@hotmail.com>
2018-08-20 22:49:24 -0300

    Adding initial projects

---
+++ img2html.baselib/imgconv.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace img2html.baselib
+{
+    public class imgconv
+    {
+        public string FileFullPath { get; set; }
+        public string LastError { get; set; }
+
+        public imgconv(string fileFullPath)
+        {
+            FileFullPath = fileFullPath;
+        }
+
+        public string GetHtml()
+        {
+            return null;
+        }
+    }
+}
---
+++ img2html.demo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace img2html.demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Usage:\n\timg2html <filename>");
+        }
+    }
+}
